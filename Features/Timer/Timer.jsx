82f6b781d2eafd668bf54e0c5c81aadd7c541c2d
BatanAndrei <yabatan777@gmail.com>
2025-10-08 14:12:31 +0300

    fix

--- Features/Timer/Timer.jsx
+++ Features/Timer/Timer.jsx
@@ -15,7 +15,7 @@ const Timer = () => {
   const [isBlinking, setIsBlinking] = useState(false);
 
   useEffect(() => {
-    if (seconds > 0 && status !== 'loading') {
+    if (seconds > 0 && status === 'succeeded') {
       const interval = setInterval(() => {
         setSeconds((s) => s - 1);
       }, 1000);
